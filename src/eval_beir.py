103ad7f8d49aa194991fa00ca00e5495299f60a6
heshanxiu <heshanxiu@g.ucla.edu>
2022-10-21 16:10:44 -0700

    add option to select path

--- src/eval_beir.py
+++ src/eval_beir.py
@@ -13,6 +13,7 @@ from sbert import SentenceTransformerA
 import os 
 from typing import List, Dict
 import numpy as np
+import sys
 
 class BEIRSbertModel:
     def __init__(self, model, tokenizer, max_length=256):
@@ -34,8 +35,8 @@ class BEIRSbertModel:
 # NOTE: this version only works for max agg in SPLADE, so the two directories below !
 # If you want to use old weights ("../weights/flops_best" and "../weights/flops_efficient") for BEIR benchmark,
 # change the SPLADE aggregation in SPLADE forward in models.py
-model_type_or_dir = "output/train_adv_bi-encoder-mnrl-distilbert-base-uncased-margin_3.0-lambda0.1-uniq0.05-2022-10-06_08-02-47/gen_iter_14/0_TransformerClassifier" 
-outfile = "beir_perf_0.1_0.05_iter14.json"
+model_type_or_dir = sys.argv[1]
+outfile = sys.argv[2]
 
 # loading model and tokenizer:
 word_embedding_model = model.TransformerClassifier(model_type_or_dir, max_seq_length=500)
@@ -48,7 +49,7 @@ tokenizer = AutoTokenizer.from_pretrained(model_type_or_dir)
 beir_model = BEIRSbertModel(model, tokenizer)
 all_results = dict()
 
-for dataset in ["trec-covid", "arguana","dbpedia-entity", "scidocs", "scifact", "webis-touche2020"]: #, "nfcorpus", "quora", "climate-fever", "fiqa", "fever", "hotpotqa", "nq"]:
+for dataset in ["trec-covid", "arguana", "dbpedia-entity", "scidocs", "scifact", "webis-touche2020"]: #, "nfcorpus", "quora", "climate-fever", "fiqa", "fever", "hotpotqa", "nq"]:
     print("start:", dataset)
     url = "https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/{}.zip".format(dataset)
     
