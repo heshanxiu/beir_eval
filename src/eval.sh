38532a26cf3fd472b195d974f3e9ab07a6d02918
heshanxiu <heshanxiu@g.ucla.edu>
2022-11-14 20:56:10 -0800

    add an example of eval script

---
+++ src/eval.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+
+CUDA_VISIBLE_DEVICES=0 nohup python -u eval_beir.py \
+      /mnt/efs/fs1/sbert/examples/training/ms_marco/output/train_bi-encoder-mnrl-distilbert-base-uncased-margin_3.0-2022-11-09_23-32-10/156000 \
+      miniLM6_dual_beir.json > miniLM6_dual_mnrl_beir.txt 2>&1 &
